eaa5083d8f52b5430da35a73a7017a825e8f83ae
Jhancarlos Lenis Arango <jahn.lenis@celuweb.com>
2023-01-23 16:13:55 -0500

    terminando funcionalidad de reproducri el radio

--- app_radio/lib/presentation/home/controller/controller_home.dart
+++ app_radio/lib/presentation/home/controller/controller_home.dart
@@ -1,13 +1,5 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:get/get.dart';
 
 class ControllerHome extends GetxController {
-  RxBool isPlay = false.obs;
-  final player = AudioPlayer();
-  final String url =
-      "https://cp.usastreams.com/pr2g/APPlayerRadioHTML5.aspx?stream=https://stream.zeno.fm/8zxrngxe0k8uv&fondo=06&formato=mp3&color=2&titulo=3&autoStart=1&vol=7&tipo=13&nombre=RADIO+RECOBRO+BOGOTA&imagen=https://static.wixstatic.com/media/06da7e_3c76b9fc9e5b4cb7ae3cbdb27e1c4b4a~mv2.png";
-
-  Future play() async {
-    await player.play(url);
-  }
+  
 }
--- app_radio/lib/presentation/home/widgets/bottom_body_home.dart
+++ app_radio/lib/presentation/home/widgets/bottom_body_home.dart
@@ -1,39 +1,81 @@
 // ignore_for_file: unrelated_type_equality_checks
 
-import 'package:app_radio/presentation/home/controller/controller_home.dart';
+import 'package:app_radio/presentation/const/color_const.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:just_audio/just_audio.dart';
 
-class BottomBodyHome extends StatelessWidget {
+class BottomBodyHome extends StatefulWidget {
   const BottomBodyHome({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<BottomBodyHome> createState() => _BottomBodyHomeState();
+}
+
+class _BottomBodyHomeState extends State<BottomBodyHome> {
+  late AudioPlayer audioPlayer;
+  final String url = "http://stream.zeno.fm/8zxrngxe0k8uv";
+
+  @override
+  void initState() {
+    super.initState();
+    audioPlayer = AudioPlayer()..setUrl(url);
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final controller = Get.put(ControllerHome());
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 20),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Obx(() => (controller.isPlay.value == false)
-              ? IconButton(
-                  iconSize: 100,
-                  onPressed: () async {
-                    controller.isPlay.value = !controller.isPlay.value;
-                    controller.play();
-                  },
-                  icon: Image.asset('assets/icons/play_icon.png'))
-              : IconButton(
-                  iconSize: 100,
-                  onPressed: () async {
-                    controller.isPlay.value = !controller.isPlay.value;
-                    controller.player.pause();
-                  },
-                  icon: Image.asset('assets/icons/pause_icon.png'))),
-        ],
+        children: [Controls(audioPlayer: audioPlayer)],
       ),
+    );
+  }
+}
+
+class Controls extends StatelessWidget {
+  const Controls({super.key, required this.audioPlayer});
+
+  final AudioPlayer audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<PlayerState>(
+      stream: audioPlayer.playerStateStream,
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        final playerState = snapshot.data;
+        final processingState = playerState?.processingState;
+        final playing = playerState?.playing;
+        if (!(playing ?? false)) {
+          return IconButton(
+            onPressed: audioPlayer.play,
+            icon: const Icon(Icons.play_arrow_rounded),
+            iconSize: 100,
+            color: ColorsConst.beish,
+          );
+        } else if (processingState != ProcessingState.completed) {
+          return IconButton(
+            onPressed: audioPlayer.pause,
+            icon: const Icon(Icons.pause_rounded),
+            iconSize: 100,
+            color: ColorsConst.beish,
+          );
+        }
+
+        return Icon(
+          Icons.play_arrow_rounded,
+          size: 100,
+          color: ColorsConst.beish,
+        );
+      },
     );
   }
 }
