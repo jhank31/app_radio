94e0dba3a8f2891a238e7bc23fd87909fb44dd92
Jhancarlos Lenis Arango <jahn.lenis@celuweb.com>
2023-06-05 11:04:37 -0500

    generando el aab

---
+++ radio_recobro_bogota/android/app/src/main/kotlin/com/bogota/radio_recobro_bogota/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bogota.radio_recobro_bogota
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- radio_recobro_bogota/android/app/src/main/kotlin/com/example/radio_recobro_bogota/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.radio_recobro_bogota
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
